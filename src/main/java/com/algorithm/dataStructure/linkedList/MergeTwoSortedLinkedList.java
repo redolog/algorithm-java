218593ca0b6f20de475eaddac65b7a9072928008
dragonsong <songhuilong1024@outlook.com>
2020-12-16 19:19:42 +0800

    feat: Add recursion version for merging two sorted linkedList

--- src/main/java/com/algorithm/dataStructure/linkedList/MergeTwoSortedLinkedList.java
+++ src/main/java/com/algorithm/dataStructure/linkedList/MergeTwoSortedLinkedList.java
@@ -51,6 +51,22 @@ public class MergeTwoSortedLinkedList {
         return head.next;
     }
 
+    private ListNode recurse(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = recurse(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = recurse(l2.next, l1);
+            return l2;
+        }
+    }
+
 }
 
 class ListNode {
