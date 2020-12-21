d9b2566d504181830d0469487d928c73e8c3fe3e
dragonsong <songhuilong1024@outlook.com>
2020-12-21 20:02:07 +0800

    feat: Add ClimbStairs fib table version

--- src/main/java/com/algorithm/dp/ClimbStairs.java
+++ src/main/java/com/algorithm/dp/ClimbStairs.java
@@ -39,7 +39,7 @@ public class ClimbStairs {
      * @return 求符合条件的组合数
      */
     public static int climbStairs(int n) {
-        int[] cache = new int[n];
+        int[] cache = new int[n + 1];
         return cache(n, cache);
     }
 
@@ -61,4 +61,19 @@ public class ClimbStairs {
         return cache[n];
     }
 
+    public int table(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] table = new int[n + 1];
+
+        //base cases
+        table[0] = 0;
+        table[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            table[i] = table[i - 1] + table[i - 2];
+        }
+        return table[n];
+    }
 }
