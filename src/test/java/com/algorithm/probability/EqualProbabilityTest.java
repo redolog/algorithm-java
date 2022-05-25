cb99d7f9f280d3b6ab51c8f079e3e32792122376
dragonsong <songhuilong1024@outlook.com>
2022-05-25 11:46:36 +0800

    test: Complete UT for rand07/rand17;

--- src/test/java/com/algorithm/probability/EqualProbabilityTest.java
+++ src/test/java/com/algorithm/probability/EqualProbabilityTest.java
@@ -39,4 +39,30 @@ public class EqualProbabilityTest {
 
         Assert.assertTrue(NumberUtils.nearlyEquals(precision, randCounts));
     }
+
+    @Test
+    public void epRand07() {
+        int[] randCounts = ProbabilityUtils.runStoreEpCnts(
+                cnt,
+                0, 7,
+                Rand2Rand::rand07
+        );
+
+        System.out.println(Arrays.toString(randCounts));
+
+        Assert.assertTrue(NumberUtils.nearlyEquals(precision, randCounts));
+    }
+    @Test
+    public void epRand17() {
+        int[] randCounts = ProbabilityUtils.runStoreEpCnts(
+                cnt,
+                1, 7,
+                Rand2Rand::rand17
+        );
+
+        System.out.println(Arrays.toString(randCounts));
+
+        Assert.assertTrue(NumberUtils.nearlyEquals(precision, randCounts));
+    }
+
 }
