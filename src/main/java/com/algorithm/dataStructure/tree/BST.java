4b17ba0b98c6712b68601cdac7529d1e44fb60a9
dragonsong <songhuilong1024@outlook.com>
2022-06-13 21:41:53 +0800

    feat: Update BST with miniNode/maxNode/contains/delete ;

--- src/main/java/com/algorithm/dataStructure/tree/BST.java
+++ src/main/java/com/algorithm/dataStructure/tree/BST.java
@@ -10,6 +10,89 @@ public class BST {
 
     public TreeNode root;
 
+    public boolean contains(int val) {
+        return contains(root, val);
+    }
+
+    private boolean contains(TreeNode curr, int val) {
+        if (curr == null) {
+            return false;
+        }
+        if (curr.val == val) {
+            return true;
+        }
+
+        if (curr.val > val) {
+            return contains(curr.left, val);
+        } else {
+            return contains(curr.right, val);
+        }
+    }
+
+    /**
+     * 传入一棵BST树，返回其最小值节点
+     */
+    public TreeNode miniNode(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        // 遍历到无左子节点的节点时，为最小节点
+        if (node.left == null) {
+            return node;
+        }
+        return miniNode(node.left);
+    }
+
+    /**
+     * 传入一棵BST树，返回其最大值节点
+     */
+    public TreeNode maxNode(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        // 遍历到无右子节点的节点时，为最大节点
+        if (node.right == null) {
+            return node;
+        }
+        return maxNode(node.right);
+    }
+
+    /**
+     * 删除BST下最小节点，返回树根
+     *
+     * @param node 当前树根节点
+     * @return 返回删除最小节点后的树根
+     */
+    public TreeNode removeMin(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        // 当前节点如果没有左子节点，说明有已经是最小
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 删除BST下最大节点，返回树根
+     *
+     * @param node 当前树根节点
+     * @return 返回删除最大节点后的树根
+     */
+    public TreeNode removeMax(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        // 当前节点如果没有右子节点，说明有已经是最大
+        if (node.right == null) {
+            return node.left;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
     /**
      * 查 val 对应BST上的节点
      * 1. 从当前节点出发，val如果等于当前节点值，直接返回节点
@@ -73,6 +156,36 @@ public class BST {
      * @param val 待删除值
      */
     public void delete(int val) {
+        delete(root, val);
+    }
+
+    /**
+     * @return 返回删除完val节点后的子树根节点
+     */
+    private TreeNode delete(TreeNode node, int val) {
+        if (node == null) {
+            return null;
+        }
+        if (node.val > val) {
+            node.left = delete(node.left, val);
+            return node;
+        } else if (node.val < val) {
+            node.right = delete(node.right, val);
+            return node;
+        } else {
+            // node.val==val
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            // val等值节点同时有左右子节点，删除右子树下的最小节点，同时将右子树最小节点替换到node的位置
+            TreeNode rightMini = new TreeNode(miniNode(node.right));
+            rightMini.right = removeMin(node.right);
+            rightMini.left = node.left;
+            return rightMini;
+        }
     }
 
 }
