c17d7a1f715b748228a0c26a706ab40fe8811323
dragonsong <songhuilong1024@outlook.com>
2022-06-15 18:18:34 +0800

    feat: Add IsValidBst with UT;

---
+++ src/main/java/com/algorithm/dataStructure/tree/IsValidBst.java
@@ -0,0 +1,73 @@
+package com.algorithm.dataStructure.tree;
+
+/**
+ * 98. 验证二叉搜索树
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 有效 二叉搜索树定义如下：
+ * <p>
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * <p>
+ * https://leetcode.cn/problems/validate-binary-search-tree/
+ *
+ * @author dragonsong  @date 2022/6/15
+ */
+public class IsValidBst {
+
+    long preVal = Long.MIN_VALUE;
+
+    /**
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 41 MB
+     * , 在所有 Java 提交中击败了
+     * 48.66%
+     * 的用户
+     * 通过测试用例：
+     * 80 / 80
+     */
+    public boolean isValidBSTInOrder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        boolean validLeft = isValidBSTInOrder(root.left);
+        if (!validLeft) {
+            return false;
+        }
+        if (preVal >= root.val) {
+            return false;
+        }
+        preVal = root.val;
+        return isValidBSTInOrder(root.right);
+    }
+
+    public boolean isValidBSTWithRange(TreeNode root) {
+        return isValidBSTWithRange(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    /**
+     * 检查bst节点值是否在开区间内
+     *
+     * @param node     当前节点
+     * @param minValue 左开区间
+     * @param maxValue 右开区间
+     */
+    private boolean isValidBSTWithRange(TreeNode node, long minValue, long maxValue) {
+        if (node == null) {
+            return true;
+        }
+        int val = node.val;
+        if (val <= minValue || val >= maxValue) {
+            return false;
+        }
+        return isValidBSTWithRange(node.left, minValue, val) && isValidBSTWithRange(node.right, val, maxValue);
+    }
+
+
+}
---
+++ src/test/java/com/algorithm/dataStructure/tree/IsValidBstTest.java
@@ -0,0 +1,23 @@
+package com.algorithm.dataStructure.tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsValidBstTest {
+    IsValidBst service = new IsValidBst();
+
+    @Test
+    public void isValidBST() {
+        Assert.assertTrue(service.isValidBSTInOrder(TreeNode.buildAnyWithArr(new Integer[]{2, 1, 3})));
+        Assert.assertFalse(service.isValidBSTInOrder(TreeNode.buildAnyWithArr(new Integer[]{5, 1, 4, null, null, 3, 6})));
+        Assert.assertFalse(service.isValidBSTInOrder(TreeNode.buildAnyWithArr(new Integer[]{5, 4, 6, null, null, 3, 7})));
+    }
+
+    @Test
+    public void isValidBSTWithRange() {
+
+        Assert.assertTrue(service.isValidBSTWithRange(TreeNode.buildAnyWithArr(new Integer[]{2, 1, 3})));
+        Assert.assertFalse(service.isValidBSTWithRange(TreeNode.buildAnyWithArr(new Integer[]{5, 1, 4, null, null, 3, 6})));
+        Assert.assertFalse(service.isValidBSTWithRange(TreeNode.buildAnyWithArr(new Integer[]{5, 4, 6, null, null, 3, 7})));
+    }
+}
