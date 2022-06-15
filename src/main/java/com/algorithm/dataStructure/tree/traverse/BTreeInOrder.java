90c1929a8c5eb57fca1a93b29f7518d1b3ab9abd
dragonsong <songhuilong1024@outlook.com>
2022-06-15 23:02:24 +0800

    doc: Update comment for inOrderStack;

--- src/main/java/com/algorithm/dataStructure/tree/traverse/BTreeInOrder.java
+++ src/main/java/com/algorithm/dataStructure/tree/traverse/BTreeInOrder.java
@@ -24,18 +24,23 @@ public class BTreeInOrder {
     }
 
     /**
-     * 整个左边界依次入栈
+     * 二叉树中序遍历
      */
     public static void inOrderStack(TreeNode node) {
+        // 使用栈结构存储调用链路
         Deque<TreeNode> stack = new LinkedList<>();
 
         while (!stack.isEmpty() || node != null) {
+            // node遍历到有值的节点，入栈，同时挪动到其左子节点，直至到最左子节点
             while (node != null) {
                 stack.push(node);
                 node = node.left;
             }
+            // 已到达最左节点，开始从栈中取出遍历过的节点
             node = stack.pop();
+            // 处理当前节点
             System.out.println(node.val);
+            // 向右侧挪动，如果没有右子节点，下一步循环会持续从栈中出栈元素，一步步退回到root节点后，开始遍历root的右子树
             node = node.right;
         }
     }
