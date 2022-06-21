fa7405c09aa81bc680778cb29de7831eb825f352
dragonsong <songhuilong1024@outlook.com>
2022-06-21 23:28:43 +0800

    feat: Add NumberOfOneBits without UT;

---
+++ src/main/java/com/algorithm/bit/NumberOfOneBits.java
@@ -0,0 +1,91 @@
+package com.algorithm.bit;
+
+/**
+ * 191. 位1的个数
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 请注意，在某些语言（如 Java）中，没有无符号整数类型。在这种情况下，输入和输出都将被指定为有符号整数类型，并且不应影响您的实现，因为无论整数是有符号的还是无符号的，其内部的二进制表示形式都是相同的。
+ * 在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在上面的 示例 3 中，输入表示有符号整数 -3。
+ * <p>
+ * https://leetcode.cn/problems/number-of-1-bits/
+ *
+ * @author dragonsong  @date 2022/6/21
+ */
+public class NumberOfOneBits {
+
+    /**
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 39.1 MB
+     * , 在所有 Java 提交中击败了
+     * 5.15%
+     * 的用户
+     * 通过测试用例：
+     * 601 / 601
+     */
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ++cnt;
+            }
+        }
+        return cnt;
+    }
+
+    /**
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 38.5 MB
+     * , 在所有 Java 提交中击败了
+     * 50.44%
+     * 的用户
+     * 通过测试用例：
+     * 601 / 601
+     */
+    public int hammingWeight2(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            cnt += n & 1;
+            n >>>= 1;
+        }
+        return cnt;
+    }
+
+    /**
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 38.8 MB
+     * , 在所有 Java 提交中击败了
+     * 12.59%
+     * 的用户
+     * <p>
+     * 消除二进制末尾的 1
+     */
+    public int hammingWeight3(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            ++cnt;
+            // 消除二进制末尾的 1
+            n &= n - 1;
+        }
+        return cnt;
+    }
+}
