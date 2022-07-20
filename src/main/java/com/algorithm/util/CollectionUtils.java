bc0a329e1a751e466eb4c315f1a31c3954113167
dragonsong <songhuilong1024@outlook.com>
2022-07-20 17:37:10 +0800

    feat: Add createTwoDimensionList in CollectionUtils;

--- src/main/java/com/algorithm/util/CollectionUtils.java
+++ src/main/java/com/algorithm/util/CollectionUtils.java
@@ -1,28 +1,86 @@
 package com.algorithm.util;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CollectionUtils {
-    private CollectionUtils(){}
+    private CollectionUtils() {
+    }
+
+    /**
+     * Returns a {@link Map} mapping each unique element in the given
+     * {@link Collection} to an {@link Integer} representing the number
+     * of occurrences of that element in the {@link Collection}.
+     * <p>
+     * Only those elements present in the collection will appear as
+     * keys in the map.
+     * </p>
+     *
+     * @param <O>  the type of object in the returned {@link Map}. This is a super type of &lt;I&gt;.
+     * @param coll the collection to get the cardinality map for, must not be null
+     * @return the populated cardinality map
+     */
+    public static <O> Map<O, Integer> getCardinalityMap(final Iterable<? extends O> coll) {
+        final Map<O, Integer> count = new HashMap<>();
+        for (O obj : coll) {
+            count.merge(obj, 1, Integer::sum);
+        }
+        return count;
+    }
+
+    public static boolean isEmpty(Collection<?> coll) {
+        return coll == null || coll.isEmpty();
+    }
+
+    public static boolean equals(Collection<?> a, Collection<?> b) {
+        if (a.size() != b.size()) {
+            return false;
+        }
+        final CardinalityHelper<Object> helper = new CardinalityHelper<>(a, b);
+        if (helper.cardinalityA.size() != helper.cardinalityB.size()) {
+            return false;
+        }
+        for (final Object obj : helper.cardinalityA.keySet()) {
+            if (helper.freqA(obj) != helper.freqB(obj)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<List<?>> createTwoDimensionList(List<?> l1, List<?> l2) {
+        List<List<?>> ans = new ArrayList<>();
+        ans.add(l1);
+        ans.add(l2);
+        return ans;
+    }
+
+    public static List<Integer> asList(int... arr) {
+        return Arrays.stream(arr).boxed().collect(Collectors.toList());
+    }
 
     /**
      * Helper class to easily access cardinality properties of two collections.
-     * @param <O>  the element type
+     *
+     * @param <O> the element type
      */
     private static class CardinalityHelper<O> {
 
-        /** Contains the cardinality for each object in collection A. */
+        /**
+         * Contains the cardinality for each object in collection A.
+         */
         final Map<O, Integer> cardinalityA;
 
-        /** Contains the cardinality for each object in collection B. */
+        /**
+         * Contains the cardinality for each object in collection B.
+         */
         final Map<O, Integer> cardinalityB;
 
         /**
          * Create a new CardinalityHelper for two collections.
-         * @param a  the first collection
-         * @param b  the second collection
+         *
+         * @param a the first collection
+         * @param b the second collection
          */
         public CardinalityHelper(final Iterable<? extends O> a, final Iterable<? extends O> b) {
             cardinalityA = CollectionUtils.<O>getCardinalityMap(a);
@@ -31,7 +89,8 @@ public class CollectionUtils {
 
         /**
          * Returns the maximum frequency of an object.
-         * @param obj  the object
+         *
+         * @param obj the object
          * @return the maximum frequency of the object
          */
         public final int max(final Object obj) {
@@ -40,7 +99,8 @@ public class CollectionUtils {
 
         /**
          * Returns the minimum frequency of an object.
-         * @param obj  the object
+         *
+         * @param obj the object
          * @return the minimum frequency of the object
          */
         public final int min(final Object obj) {
@@ -49,7 +109,8 @@ public class CollectionUtils {
 
         /**
          * Returns the frequency of this object in collection A.
-         * @param obj  the object
+         *
+         * @param obj the object
          * @return the frequency of the object in collection A
          */
         public int freqA(final Object obj) {
@@ -58,7 +119,8 @@ public class CollectionUtils {
 
         /**
          * Returns the frequency of this object in collection B.
-         * @param obj  the object
+         *
+         * @param obj the object
          * @return the frequency of the object in collection B
          */
         public int freqB(final Object obj) {
@@ -72,47 +134,6 @@ public class CollectionUtils {
             }
             return 0;
         }
-    }
-
-    /**
-     * Returns a {@link Map} mapping each unique element in the given
-     * {@link Collection} to an {@link Integer} representing the number
-     * of occurrences of that element in the {@link Collection}.
-     * <p>
-     * Only those elements present in the collection will appear as
-     * keys in the map.
-     * </p>
-     *
-     * @param <O>  the type of object in the returned {@link Map}. This is a super type of &lt;I&gt;.
-     * @param coll  the collection to get the cardinality map for, must not be null
-     * @return the populated cardinality map
-     */
-    public static <O> Map<O, Integer> getCardinalityMap(final Iterable<? extends O> coll) {
-        final Map<O, Integer> count = new HashMap<>();
-        for (O obj : coll) {
-            count.merge(obj, 1, Integer::sum);
-        }
-        return count;
-    }
-
-    public static boolean isEmpty(Collection<?> coll){
-        return coll == null || coll.isEmpty();
-    }
-
-    public static boolean equals(Collection<?> a,Collection<?> b){
-        if(a.size() != b.size()) {
-            return false;
-        }
-        final CardinalityHelper<Object> helper = new CardinalityHelper<>(a, b);
-        if(helper.cardinalityA.size() != helper.cardinalityB.size()) {
-            return false;
-        }
-        for( final Object obj : helper.cardinalityA.keySet()) {
-            if(helper.freqA(obj) != helper.freqB(obj)) {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
