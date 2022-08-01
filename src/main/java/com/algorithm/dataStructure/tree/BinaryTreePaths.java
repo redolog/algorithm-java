9495eb5e5cd96749e2e5fdedd8e5a90c4c38974e
dragonsong <songhuilong1024@outlook.com>
2022-08-01 13:20:04 +0800

    feat: Add BinaryTreePaths dfs solution with UT;

---
+++ src/main/java/com/algorithm/dataStructure/tree/BinaryTreePaths.java
@@ -0,0 +1,89 @@
+package com.algorithm.dataStructure.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 257. 二叉树的所有路径
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3,null,5]
+ * 输出：["1->2->5","1->3"]
+ * 示例 2：
+ * <p>
+ * 输入：root = [1]
+ * 输出：["1"]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点的数目在范围 [1, 100] 内
+ * -100 <= Node.val <= 100
+ * <p>
+ * https://leetcode.cn/problems/binary-tree-paths/
+ *
+ * @author dragonsong  @date 2022/8/1
+ */
+public class BinaryTreePaths {
+
+    static class DFS {
+        /*
+         * 执行用时：
+         * 1 ms
+         * , 在所有 Java 提交中击败了
+         * 99.97%
+         * 的用户
+         * 内存消耗：
+         * 40.3 MB
+         * , 在所有 Java 提交中击败了
+         * 99.14%
+         * 的用户
+         * 通过测试用例：
+         * 208 / 208
+         */
+
+        List<String> ans;
+
+        StringBuilder builder;
+
+        public List<String> binaryTreePaths(TreeNode root) {
+            ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            builder = new StringBuilder();
+            dfs(root);
+            return ans;
+        }
+
+        private void dfs(TreeNode node) {
+            builder.append(node.val);
+            if (node.left == null && node.right == null) {
+                ans.add(builder.toString());
+                return;
+            }
+
+            if (node.left != null) {
+                builder.append("->");
+                dfs(node.left);
+                if (builder.indexOf("->") != -1) {
+                    builder.delete(builder.lastIndexOf("->"), builder.length());
+                }
+            }
+            if (node.right != null) {
+                builder.append("->");
+                dfs(node.right);
+                if (builder.indexOf("->") != -1) {
+                    builder.delete(builder.lastIndexOf("->"), builder.length());
+                }
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/algorithm/dataStructure/tree/BinaryTreePathsTest.java
@@ -0,0 +1,22 @@
+package com.algorithm.dataStructure.tree;
+
+import com.algorithm.util.CollectionUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class BinaryTreePathsTest {
+
+    @Test
+    public void binaryTreePaths() {
+        BinaryTreePaths.DFS dfs = new BinaryTreePaths.DFS();
+        List<String> ans1 = dfs.binaryTreePaths(TreeNode.buildAnyWithArr(new Integer[]{1, 2, 3, null, 5}));
+        assertTrue(CollectionUtils.equals(Arrays.asList("1->2->5", "1->3"), ans1));
+        assertTrue(CollectionUtils.equals(Collections.singletonList("1"), dfs.binaryTreePaths(TreeNode.buildAnyWithArr(new Integer[]{1}))));
+        assertTrue(CollectionUtils.equals(Arrays.asList("1->2->5", "1->2->6", "1->3"), dfs.binaryTreePaths(TreeNode.buildAnyWithArr(new Integer[]{1, 2, 3, 5, 6}))));
+    }
+}
