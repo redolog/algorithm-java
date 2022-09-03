9de016b04f1457a79a868faca6c694247edb69da
redolog <songhuilong1024@outlook.com>
2022-09-03 09:00:24 +0800

    test: Additional UT for AddToArrayFormOfIntegerTest;

--- src/test/java/com/algorithm/decimal/AddToArrayFormOfIntegerTest.java
+++ src/test/java/com/algorithm/decimal/AddToArrayFormOfIntegerTest.java
@@ -16,5 +16,7 @@ public class AddToArrayFormOfIntegerTest {
         assertTrue(CollectionUtils.equals(Arrays.asList(4, 5, 5), service.addToArrayForm(new int[]{2, 7, 4}, 181)));
         assertTrue(CollectionUtils.equals(Arrays.asList(1, 0, 2, 1), service.addToArrayForm(new int[]{2, 1, 5}, 806)));
         assertTrue(CollectionUtils.equals(Arrays.asList(1, 0, 0, 0, 0), service.addToArrayForm(new int[]{0}, 10000)));
+        assertTrue(CollectionUtils.equals(Arrays.asList(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), service.addToArrayForm(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}, 1)));
+        assertTrue(CollectionUtils.equals(Arrays.asList(1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 5, 7, 9), service.addToArrayForm(new int[]{1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3}, 516)));
     }
 }
