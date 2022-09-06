3c0c7b3066c8cf8d5f4f8e3e18bc4a72d307c8ee
redolog <songhuilong1024@outlook.com>
2022-09-06 10:25:31 +0800

    feat: Update DefuseTheBomb PreSum solution with UT;

--- src/main/java/com/algorithm/dataStructure/tree/DefuseTheBomb.java
+++ src/main/java/com/algorithm/dataStructure/tree/DefuseTheBomb.java
@@ -7,6 +7,57 @@ package com.algorithm.dataStructure.tree;
  */
 public class DefuseTheBomb {
 
+    static class PreSum {
+        /**
+         * 预处理成前缀和，计算区间和时直接用前缀和加减可得，单次求和复杂度 O(1)
+         * 整体时间复杂度：O(n)
+         * <p>
+         * 无额外空间开销，空间复杂度：O(1)
+         */
+        public int[] decrypt(int[] code, int k) {
+            int preSum = 0;
+            int n = code.length;
+            int[] ans = new int[n];
+            if (k == 0) {
+                return ans;
+            }
+            // k负数，取相反数，数组反转
+            boolean negativeK = k < 0;
+            if (negativeK) {
+                k = -k;
+                reverse(code);
+            }
+            for (int i = 0; i < n; i++) {
+                preSum += code[i];
+                code[i] = preSum;
+            }
+            for (int i = 0; i < n; i++) {
+                if (i + k < n) {
+                    ans[i] = code[i + k] - code[i];
+                } else {
+                    ans[i] = code[n - 1] - code[i] + code[i + k - n];
+                }
+            }
+            if (negativeK) {
+                reverse(ans);
+            }
+            return ans;
+        }
+
+        private void reverse(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n / 2; i++) {
+                swap(arr, i, n - i - 1);
+            }
+        }
+
+        private void swap(int[] arr, int aIdx, int bIdx) {
+            int tmp = arr[aIdx];
+            arr[aIdx] = arr[bIdx];
+            arr[bIdx] = tmp;
+        }
+    }
+
     static class BF {
         /**
          * 时间复杂度 O(n*k)
--- src/test/java/com/algorithm/dataStructure/tree/DefuseTheBombTest.java
+++ src/test/java/com/algorithm/dataStructure/tree/DefuseTheBombTest.java
@@ -12,6 +12,9 @@ public class DefuseTheBombTest {
         assertArrayEquals(new int[]{12, 10, 16, 13}, bf.decrypt(new int[]{5, 7, 1, 4}, 3));
         assertArrayEquals(new int[]{0, 0, 0, 0}, bf.decrypt(new int[]{1, 2, 3, 4}, 0));
         assertArrayEquals(new int[]{12, 5, 6, 13}, bf.decrypt(new int[]{2, 4, 9, 3}, -2));
-
+        DefuseTheBomb.PreSum preSum = new DefuseTheBomb.PreSum();
+        assertArrayEquals(new int[]{12, 10, 16, 13}, preSum.decrypt(new int[]{5, 7, 1, 4}, 3));
+        assertArrayEquals(new int[]{0, 0, 0, 0}, preSum.decrypt(new int[]{1, 2, 3, 4}, 0));
+        assertArrayEquals(new int[]{12, 5, 6, 13}, preSum.decrypt(new int[]{2, 4, 9, 3}, -2));
     }
 }
