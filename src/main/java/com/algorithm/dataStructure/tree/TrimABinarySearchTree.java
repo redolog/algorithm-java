fac732b5a6963d742eddfd2cd7e547ad6e42478d
redolog <songhuilong1024@outlook.com>
2022-09-10 09:11:15 +0800

    feat: Add TrimABinarySearchTree with UT;

---
+++ src/main/java/com/algorithm/dataStructure/tree/TrimABinarySearchTree.java
@@ -0,0 +1,40 @@
+package com.algorithm.dataStructure.tree;
+
+/**
+ * https://leetcode.cn/problems/trim-a-binary-search-tree/
+ *
+ * @author DragonSong  @date 2022/9/10
+ */
+public class TrimABinarySearchTree {
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        // 根小于low，直接进入右子树
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        // 大于则进入左子树
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        dfs(root, low, high);
+        return root;
+    }
+
+    private void dfs(TreeNode node, int low, int high) {
+        if (null == node) {
+            return;
+        }
+        // 以 3,0,4,null,2,null,null,1 为例，此处需消除掉0节点
+        if (node.left != null && node.left.val < low) {
+            node.left = trimBST(node.left, low, high);
+        }
+        if (node.right != null && node.right.val > high) {
+            node.right = trimBST(node.right, low, high);
+        }
+        dfs(node.left, low, high);
+        dfs(node.right, low, high);
+    }
+}
---
+++ src/test/java/com/algorithm/dataStructure/tree/TrimABinarySearchTreeTest.java
@@ -0,0 +1,15 @@
+package com.algorithm.dataStructure.tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrimABinarySearchTreeTest {
+
+    @Test
+    public void trimBST() {
+        TrimABinarySearchTree service = new TrimABinarySearchTree();
+        assertEquals(TreeNode.buildAny(1, null, 2), service.trimBST(TreeNode.buildAny(1, 0, 2), 1, 2));
+        assertEquals(TreeNode.buildAny(3, 2, null, 1), service.trimBST(TreeNode.buildAny(3, 0, 4, null, 2, null, null, 1), 1, 3));
+    }
+}
