da9c66b5f0dfebef8384def105483d4fb38d3b1a
redolog <songhuilong1024@outlook.com>
2022-09-18 21:02:54 +0800

    feat: Update GetKLeastNumbers QuickSort solution with UT;

--- src/main/java/com/algorithm/lookup/GetKLeastNumbers.java
+++ src/main/java/com/algorithm/lookup/GetKLeastNumbers.java
@@ -9,6 +9,67 @@ import java.util.*;
  */
 public class GetKLeastNumbers {
 
+    static class QuickSort {
+
+        /**
+         * topK 问题，使用快排变体：快速选择算法
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(logn)
+         */
+        public int[] getLeastNumbers(int[] arr, int k) {
+            if (k == 0) {
+                return new int[0];
+            }
+            // [0,k-1] 为题目所求范围
+            return quickSelect(arr, 0, arr.length - 1, k - 1);
+        }
+
+        private int[] quickSelect(int[] arr, int l, int r, int k) {
+            int pIdx = partition(arr, l, r);
+            if (pIdx == k) {
+                return Arrays.copyOf(arr, k + 1);
+            }
+            return pIdx < k ? quickSelect(arr, pIdx + 1, r, k) : quickSelect(arr, l, pIdx - 1, k);
+        }
+
+        /**
+         * 快排核心分区算法：随机选取一个数，左右分别排列为小于、大于这个数的序列
+         */
+        private int partition(int[] arr, int l, int r) {
+            // 防止最右一直极值，随机化替换一下
+            int randomIdx = randomIndexWithRange(l, r);
+            swap(arr, randomIdx, r);
+            int pivotVal = arr[r], lBorder = l, rBorder = r;
+            while (true) {
+                // l查找小于 pivotVal的值
+                while (l < rBorder && arr[l] <= pivotVal) {
+                    l++;
+                }
+                // r查找大于 pivotVal的值
+                while (r > lBorder && arr[r] >= pivotVal) {
+                    r--;
+                }
+                if (l >= r) {
+                    break;
+                }
+                swap(arr, l, r);
+            }
+            // rBorder为pivot所在位置，l此时已经到了大于pivot的位置，交换，使得 arr[l] == pivotVal
+            swap(arr, l, rBorder);
+            return l;
+        }
+
+        private int randomIndexWithRange(int leftIdx, int rightIdx) {
+            return (int) (Math.random() * (rightIdx - leftIdx + 1) + leftIdx);
+        }
+
+        private void swap(int[] arr, int a, int b) {
+            int tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        }
+    }
+
     static class TreeMapSolution {
         /**
          * 使用treeMap对元素排序、计数
--- src/test/java/com/algorithm/lookup/GetKLeastNumbersTest.java
+++ src/test/java/com/algorithm/lookup/GetKLeastNumbersTest.java
@@ -1,8 +1,14 @@
 package com.algorithm.lookup;
 
+import com.algorithm.util.ArrayUtils;
+import com.algorithm.util.CollectionUtils;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GetKLeastNumbersTest {
 
@@ -19,5 +25,9 @@ public class GetKLeastNumbersTest {
         assertArrayEquals(new int[]{1, 2}, treeMapSolution.getLeastNumbers(new int[]{3, 2, 1}, 2));
         assertArrayEquals(new int[]{0}, treeMapSolution.getLeastNumbers(new int[]{0, 1, 2, 1}, 1));
         assertArrayEquals(new int[]{0, 0, 0, 1, 3, 4, 5, 6, 7}, treeMapSolution.getLeastNumbers(new int[]{0, 0, 1, 3, 4, 5, 0, 7, 6, 7}, 9));
+        GetKLeastNumbers.QuickSort quickSort = new GetKLeastNumbers.QuickSort();
+        assertTrue(CollectionUtils.equals(Arrays.asList(2, 1), ArrayUtils.intArr2List(quickSort.getLeastNumbers(new int[]{3, 2, 1}, 2))));
+        assertTrue(CollectionUtils.equals(Collections.singletonList(0), ArrayUtils.intArr2List(quickSort.getLeastNumbers(new int[]{0, 1, 2, 1}, 1))));
+        assertTrue(CollectionUtils.equals(Arrays.asList(0, 0, 0, 1, 3, 4, 5, 6, 7), ArrayUtils.intArr2List(quickSort.getLeastNumbers(new int[]{0, 0, 1, 3, 4, 5, 0, 7, 6, 7}, 9))));
     }
 }
