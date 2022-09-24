8a1c53040a8296da582a30293f6c922d612fa8f0
redolog <songhuilong1024@outlook.com>
2022-09-24 08:05:18 +0800

    feat: Update DefuseTheBomb with SlidingWindow solution;

--- src/main/java/com/algorithm/dataStructure/tree/DefuseTheBomb.java
+++ src/main/java/com/algorithm/dataStructure/tree/DefuseTheBomb.java
@@ -1,11 +1,47 @@
 package com.algorithm.dataStructure.tree;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.cn/problems/defuse-the-bomb/
  *
  * @author songhuilong  @date 2022/9/5
  */
 public class DefuseTheBomb {
+
+    static class SlidingWindow {
+        /**
+         * 官解滑动窗口
+         * https://leetcode.cn/problems/defuse-the-bomb/solution/chai-zha-dan-by-leetcode-solution-01x3/
+         */
+        public int[] decrypt(int[] code, int k) {
+            int n = code.length;
+            // 二倍code，方便直接取前置元素（取模亦可）
+            if (k == 0) {
+                return new int[n];
+            }
+            int[] largeCode = new int[n * 2];
+            for (int i = 0; i < n; i++) {
+                largeCode[i] = code[i];
+                largeCode[i + n] = code[i];
+            }
+            int l = k > 0 ? 1 : n + k;
+            int r = k > 0 ? k : n - 1;
+            int windowSum = 0;
+            for (int i = l; i <= r; i++) {
+                windowSum += largeCode[i];
+            }
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++) {
+                ans[i] = windowSum;
+                windowSum -= largeCode[l];
+                windowSum += largeCode[r + 1];
+                l++;
+                r++;
+            }
+            return ans;
+        }
+    }
 
     static class PreSum {
         /**
--- src/test/java/com/algorithm/dataStructure/tree/DefuseTheBombTest.java
+++ src/test/java/com/algorithm/dataStructure/tree/DefuseTheBombTest.java
@@ -16,5 +16,9 @@ public class DefuseTheBombTest {
         assertArrayEquals(new int[]{12, 10, 16, 13}, preSum.decrypt(new int[]{5, 7, 1, 4}, 3));
         assertArrayEquals(new int[]{0, 0, 0, 0}, preSum.decrypt(new int[]{1, 2, 3, 4}, 0));
         assertArrayEquals(new int[]{12, 5, 6, 13}, preSum.decrypt(new int[]{2, 4, 9, 3}, -2));
+        DefuseTheBomb.SlidingWindow slidingWindow = new DefuseTheBomb.SlidingWindow();
+        assertArrayEquals(new int[]{12, 10, 16, 13}, slidingWindow.decrypt(new int[]{5, 7, 1, 4}, 3));
+        assertArrayEquals(new int[]{0, 0, 0, 0}, slidingWindow.decrypt(new int[]{1, 2, 3, 4}, 0));
+        assertArrayEquals(new int[]{12, 5, 6, 13}, slidingWindow.decrypt(new int[]{2, 4, 9, 3}, -2));
     }
 }
