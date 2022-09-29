f3fe5169a8c963cc6a682bcbcc7401f0dbfba9ff
redolog <songhuilong1024@outlook.com>
2022-09-29 10:27:10 +0800

    feat: Update IntersectionOfTwoArraysII with sort solution;

--- src/main/java/com/algorithm/dataStructure/array/intersection/IntersectionOfTwoArraysII.java
+++ src/main/java/com/algorithm/dataStructure/array/intersection/IntersectionOfTwoArraysII.java
@@ -40,29 +40,73 @@ import java.util.stream.Collectors;
  */
 public class IntersectionOfTwoArraysII {
 
-    /**
-     * 时间复杂度：完整遍历两个数组一次，O(n)
-     * 空间复杂度：使用了额外的list、map  O(n)
-     */
-    public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> num2Cnt1 = Arrays.stream(nums1).boxed().collect(Collectors.toMap(num -> num, k -> 1, Integer::sum));
-        Map<Integer, Integer> num2Cnt2 = Arrays.stream(nums2).boxed().collect(Collectors.toMap(num -> num, k -> 1, Integer::sum));
-
-        List<Integer> ansList = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : num2Cnt1.entrySet()) {
-            Integer num = entry.getKey();
-            Integer cnt = entry.getValue();
-            if (num2Cnt2.containsKey(num)) {
-                int minCnt = Math.min(cnt, num2Cnt2.get(num));
-                for (int i = 0; i < minCnt; i++) {
-                    ansList.add(num);
+    static class SortSolution {
+        /**
+         * 将入参数组排序，按照合并有序数组的过程来处理
+         * <p>
+         * 时间复杂度：排序开销最大，O(n*logn)
+         * 空间复杂度：使用了额外的list  O(n)
+         */
+        public int[] intersect(int[] nums1, int[] nums2) {
+            // nums1更小
+            if (nums1.length > nums2.length) {
+                return intersect(nums2, nums1);
+            }
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+            // i遍历nums1，j遍历nums2
+            int i = 0, j = 0;
+            List<Integer> intersectionList = new ArrayList<>();
+            while (i < nums1.length && j < nums2.length) {
+                if (nums2[j] > nums1[i]) {
+                    // 小数组当前元素不在nums2中，看nums1下一个元素
+                    i++;
+                } else if (nums2[j] < nums1[i]) {
+                    // 大数组当前元素比nums1当前元素还小，看nums2下一个元素
+                    j++;
+                } else {
+                    // nums2[j]==nums1[i]
+                    // 两个数组元素相等，为交集元素
+                    intersectionList.add(nums1[i]);
+                    i++;
+                    j++;
                 }
             }
+
+            int[] ans = new int[intersectionList.size()];
+            for (int x = 0; x < intersectionList.size(); x++) {
+                ans[x] = intersectionList.get(x);
+            }
+            return ans;
         }
-        int[] ans = new int[ansList.size()];
-        for (int i = 0; i < ansList.size(); i++) {
-            ans[i] = ansList.get(i);
-        }
-        return ans;
     }
+
+    static class MapSolution {
+        /**
+         * 时间复杂度：完整遍历两个数组一次，O(n)
+         * 空间复杂度：使用了额外的list、map  O(n)
+         */
+        public int[] intersect(int[] nums1, int[] nums2) {
+            Map<Integer, Integer> num2Cnt1 = Arrays.stream(nums1).boxed().collect(Collectors.toMap(num -> num, k -> 1, Integer::sum));
+            Map<Integer, Integer> num2Cnt2 = Arrays.stream(nums2).boxed().collect(Collectors.toMap(num -> num, k -> 1, Integer::sum));
+
+            List<Integer> ansList = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : num2Cnt1.entrySet()) {
+                Integer num = entry.getKey();
+                Integer cnt = entry.getValue();
+                if (num2Cnt2.containsKey(num)) {
+                    int minCnt = Math.min(cnt, num2Cnt2.get(num));
+                    for (int i = 0; i < minCnt; i++) {
+                        ansList.add(num);
+                    }
+                }
+            }
+            int[] ans = new int[ansList.size()];
+            for (int i = 0; i < ansList.size(); i++) {
+                ans[i] = ansList.get(i);
+            }
+            return ans;
+        }
+    }
+
 }
--- src/test/java/com/algorithm/dataStructure/array/intersection/IntersectionOfTwoArraysIITest.java
+++ src/test/java/com/algorithm/dataStructure/array/intersection/IntersectionOfTwoArraysIITest.java
@@ -8,8 +8,11 @@ public class IntersectionOfTwoArraysIITest {
 
     @Test
     public void intersect() {
-        IntersectionOfTwoArraysII service = new IntersectionOfTwoArraysII();
-        assertArrayEquals(new int[]{2, 2}, service.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
-        assertArrayEquals(new int[]{4, 9}, service.intersect(new int[]{9, 4, 9, 8, 4}, new int[]{4, 9, 5}));
+        IntersectionOfTwoArraysII.MapSolution mapSolution = new IntersectionOfTwoArraysII.MapSolution();
+        assertArrayEquals(new int[]{2, 2}, mapSolution.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        assertArrayEquals(new int[]{4, 9}, mapSolution.intersect(new int[]{9, 4, 9, 8, 4}, new int[]{4, 9, 5}));
+        IntersectionOfTwoArraysII.SortSolution sortSolution = new IntersectionOfTwoArraysII.SortSolution();
+        assertArrayEquals(new int[]{2, 2}, sortSolution.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        assertArrayEquals(new int[]{4, 9}, sortSolution.intersect(new int[]{9, 4, 9, 8, 4}, new int[]{4, 9, 5}));
     }
 }
