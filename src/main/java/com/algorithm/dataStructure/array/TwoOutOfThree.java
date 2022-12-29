f56c9b9832db9c2da7e0e40e0a8b5bcb668be465
redolog <songhuilong1024@outlook.com>
2022-12-29 19:45:24 +0800

    feat: Update TwoOutOfThree with bit offset solution;

--- src/main/java/com/algorithm/dataStructure/array/TwoOutOfThree.java
+++ src/main/java/com/algorithm/dataStructure/array/TwoOutOfThree.java
@@ -13,25 +13,51 @@ import java.util.stream.Collectors;
  */
 public class TwoOutOfThree {
 
-    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-        int[] cntArr = new int[101];
-        calcDistinctCnt(nums1, cntArr);
-        calcDistinctCnt(nums2, cntArr);
-        calcDistinctCnt(nums3, cntArr);
+    static class BitSolution {
+        public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+            // 以二进制位计数，相同数在相同数组中只计数一次，如nums1 在 cntArr[num] |= bitOffset 上做标记
+            int[] cntArr = new int[101];
+            calcDistinctCnt(nums1, cntArr, 1);
+            calcDistinctCnt(nums2, cntArr, 2);
+            calcDistinctCnt(nums3, cntArr, 4);
 
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i < cntArr.length; i++) {
-            if (cntArr[i] >= 2) {
-                ans.add(i);
+            List<Integer> ans = new ArrayList<>();
+            for (int i = 1; i < cntArr.length; i++) {
+                if ((cntArr[i] & (cntArr[i] - 1)) > 1) {
+                    ans.add(i);
+                }
+            }
+            return ans;
+        }
+
+        private void calcDistinctCnt(int[] nums, int[] cntArr, int bitOffset) {
+            for (Integer num : nums) {
+                cntArr[num] = cntArr[num] | bitOffset;
             }
         }
-        return ans;
     }
 
-    private void calcDistinctCnt(int[] nums, int[] cntArr) {
-        Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-        for (Integer num : numSet) {
-            cntArr[num]++;
+    static class SetSolution {
+        public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+            int[] cntArr = new int[101];
+            calcDistinctCnt(nums1, cntArr);
+            calcDistinctCnt(nums2, cntArr);
+            calcDistinctCnt(nums3, cntArr);
+
+            List<Integer> ans = new ArrayList<>();
+            for (int i = 1; i < cntArr.length; i++) {
+                if (cntArr[i] >= 2) {
+                    ans.add(i);
+                }
+            }
+            return ans;
+        }
+
+        private void calcDistinctCnt(int[] nums, int[] cntArr) {
+            Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+            for (Integer num : numSet) {
+                cntArr[num]++;
+            }
         }
     }
 }
--- src/test/java/com/algorithm/dataStructure/array/TwoOutOfThreeTest.java
+++ src/test/java/com/algorithm/dataStructure/array/TwoOutOfThreeTest.java
@@ -12,9 +12,13 @@ public class TwoOutOfThreeTest {
 
     @Test
     public void twoOutOfThree() {
-        TwoOutOfThree service = new TwoOutOfThree();
-        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3), service.twoOutOfThree(new int[]{1, 1, 3, 2}, new int[]{2, 3}, new int[]{3})));
-        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3, 1), service.twoOutOfThree(new int[]{3, 1}, new int[]{2, 3}, new int[]{1, 2})));
-        assertTrue(CollectionUtils.equals(Collections.emptyList(), service.twoOutOfThree(new int[]{1, 2, 2}, new int[]{4, 3, 3}, new int[]{5})));
+        TwoOutOfThree.SetSolution setSolution = new TwoOutOfThree.SetSolution();
+        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3), setSolution.twoOutOfThree(new int[]{1, 1, 3, 2}, new int[]{2, 3}, new int[]{3})));
+        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3, 1), setSolution.twoOutOfThree(new int[]{3, 1}, new int[]{2, 3}, new int[]{1, 2})));
+        assertTrue(CollectionUtils.equals(Collections.emptyList(), setSolution.twoOutOfThree(new int[]{1, 2, 2}, new int[]{4, 3, 3}, new int[]{5})));
+        TwoOutOfThree.BitSolution bitSolution = new TwoOutOfThree.BitSolution();
+        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3), bitSolution.twoOutOfThree(new int[]{1, 1, 3, 2}, new int[]{2, 3}, new int[]{3})));
+        assertTrue(CollectionUtils.equals(Arrays.asList(2, 3, 1), bitSolution.twoOutOfThree(new int[]{3, 1}, new int[]{2, 3}, new int[]{1, 2})));
+        assertTrue(CollectionUtils.equals(Collections.emptyList(), bitSolution.twoOutOfThree(new int[]{1, 2, 2}, new int[]{4, 3, 3}, new int[]{5})));
     }
 }
