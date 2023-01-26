ec85311832d8bc664f1f7971495357c35032d8ec
redolog <songhuilong1024@outlook.com>
2023-01-27 03:12:37 +0800

    style: Update GreatestEnglishLetterInUpperAndLowerCase with BitSolution2 and ArrCntSolution;

--- src/main/java/com/algorithm/string/GreatestEnglishLetterInUpperAndLowerCase.java
+++ src/main/java/com/algorithm/string/GreatestEnglishLetterInUpperAndLowerCase.java
@@ -10,10 +10,6 @@ import java.util.Set;
  */
 public class GreatestEnglishLetterInUpperAndLowerCase {
 
-    public static void main(String[] args) {
-        System.out.println(Math.pow(2, 32) == (1L << 32));
-    }
-
     static class BitSolution {
         /**
          * A:65  Z:90  a:97  z:122
@@ -67,6 +63,41 @@ public class GreatestEnglishLetterInUpperAndLowerCase {
                 }
             }
             return ans == -1 ? "" : String.valueOf((char) ans);
+        }
+    }
+
+    static class ArrCntSolution {
+        public String greatestLetter(String s) {
+            int[] letterCnt = new int[64];
+            int n = s.length();
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                letterCnt[c - 'A']++;
+            }
+
+            for (int i = 25; i >= 0; i--) {
+                if (letterCnt[i] > 0 && letterCnt[i + 32] > 0) {
+                    return String.valueOf((char) (i + 'A'));
+                }
+            }
+            return "";
+        }
+    }
+
+    static class BitSolution2 {
+        public String greatestLetter(String s) {
+            long int64 = 0L;
+            int n = s.length();
+            for (int i = 0; i < n; i++) {
+                int64 |= 1L << (s.charAt(i) - 'A');
+            }
+            for (int i = 25; i >= 0; i--) {
+                // 32 为 A->a的ascii距离
+                if ((int64 & 1L << i) > 0 && (int64 & 1L << (i + 32)) > 0) {
+                    return String.valueOf((char) (i + 'A'));
+                }
+            }
+            return "";
         }
     }
 }
--- src/test/java/com/algorithm/string/GreatestEnglishLetterInUpperAndLowerCaseTest.java
+++ src/test/java/com/algorithm/string/GreatestEnglishLetterInUpperAndLowerCaseTest.java
@@ -16,5 +16,13 @@ public class GreatestEnglishLetterInUpperAndLowerCaseTest {
         assertEquals("E", bitSolution.greatestLetter("lEeTcOdE"));
         assertEquals("R", bitSolution.greatestLetter("arRAzFif"));
         assertEquals("", bitSolution.greatestLetter("AbCdEfGhIjK"));
+        GreatestEnglishLetterInUpperAndLowerCase.ArrCntSolution arrCntSolution = new GreatestEnglishLetterInUpperAndLowerCase.ArrCntSolution();
+        assertEquals("E", arrCntSolution.greatestLetter("lEeTcOdE"));
+        assertEquals("R", arrCntSolution.greatestLetter("arRAzFif"));
+        assertEquals("", arrCntSolution.greatestLetter("AbCdEfGhIjK"));
+        GreatestEnglishLetterInUpperAndLowerCase.BitSolution2 bitSolution2 = new GreatestEnglishLetterInUpperAndLowerCase.BitSolution2();
+        assertEquals("E", bitSolution2.greatestLetter("lEeTcOdE"));
+        assertEquals("R", bitSolution2.greatestLetter("arRAzFif"));
+        assertEquals("", bitSolution2.greatestLetter("AbCdEfGhIjK"));
     }
 }
